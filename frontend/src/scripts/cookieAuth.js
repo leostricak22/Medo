bbb378ffbd5cb2b5427bcab22a21b6e032a32093
Leo <stricak.leo@gmail.com>
2023-11-25 01:55:06 +0100

    Cookie page validation

--- frontend/src/scripts/cookieAuth.js
+++ frontend/src/scripts/cookieAuth.js
@@ -1,38 +1,42 @@
+function redirect_to_landing() {
+  let url = location.protocol + '//' + location.host + location.pathname
+
+  if (!(url.endsWith("/landing.html") || url.endsWith("/login.html")))
+    window.location.replace("../landing/landing.html")
+}
+
+function redirect_to_dashboard() {
+  let url = location.protocol + '//' + location.host + location.pathname
+
+  if (url.endsWith("/landing.html") || url.endsWith("/login.html"))
+    window.location.replace("../dashboard/dashboard.html")
+}
+
 function getCookie(name) {
-  // Split cookie string and get all individual name=value pairs in an array
   var cookieArr = document.cookie.split(';');
 
-  // Loop through the array elements
   for (var i = 0; i < cookieArr.length; i++) {
     var cookiePair = cookieArr[i].split('=');
 
-    /* Removing whitespace at the beginning of the cookie name
-    and compare it with the given string */
     if (name == cookiePair[0].trim()) {
-      // Decode the cookie value and return
       return decodeURIComponent(cookiePair[1]);
     }
   }
 
-  // Return null if not found
   return null;
 }
 
 function checkCookie() {
-  // Get the cookie using the function above
   var userCookie = getCookie('uuid');
 
   if (userCookie) {
-    // If the cookie exists, you might want to validate its value with the
-    // server
     console.log('Cookie found! Value is: ' + userCookie);
+    redirect_to_dashboard()
   } else {
-    // No cookie found
-    console.log('Cookie not found!');
+    redirect_to_landing();
   }
 }
 
-// Run the checkCookie function to check for the cookie
 checkCookie();
 
 
@@ -48,8 +52,9 @@ function validateCookie(cookieValue) {
       .then(data => {
         if (data.access === "granted") {
           console.log('Cookie is valid');
+          redirect_to_dashboard()
         } else {
-          console.log('Cookie is not valid');
+          redirect_to_landing();
         }
       })
       .catch(error => {
