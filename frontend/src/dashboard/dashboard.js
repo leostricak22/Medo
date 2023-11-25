d811650c0708aff4a7daf1fa66826b1d450eb609
BrunoPohovski <bruno.pohovski@gmail.com>
2023-11-25 18:01:50 +0100

    Big Boy Commit

---
+++ frontend/src/dashboard/dashboard.js
@@ -0,0 +1,20 @@
+// Description: This file contains the functions for the dashboard page
+function showCalendar() {
+  document.getElementById("calendar").innerHTML =
+    "Calendar view content goes here";
+}
+
+function showPatientView() {
+  document.getElementById("calendar").innerHTML =
+    "Patient view content goes here";
+}
+
+function logout() {
+  alert("Logout button clicked");
+  // You can add logout functionality here
+}
+
+function goToProfile() {
+  alert("Navigate to profile page");
+  // You can add profile navigation functionality here
+}
